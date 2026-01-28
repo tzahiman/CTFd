773a51ff78f7114db7270a327c4f0cee49ff1f37
Tzahi Grodzevsky <tzahiman1@gmail.com>
2026-01-28 18:39:15 +0200

    Add igw

--- terraform/modules/networking/main.tf
+++ terraform/modules/networking/main.tf
@@ -14,6 +14,22 @@ resource "aws_subnet" "public" {
   map_public_ip_on_launch = true
 }
 
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.ctf_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = { Name = "ctf-public-rt" }
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
 resource "aws_security_group" "ctf_sg" {
   name   = "ctf-sg"
   vpc_id = aws_vpc.ctf_vpc.id
